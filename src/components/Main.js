4de16a476010fa5169c41cf585a84b5c03779e35
firyang <firyang@aliyun.com>
2018-07-09 09:52:14 +0800

    output images

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,9 @@ import React from 'react';
 
 var imgDatas = require('../data/data.json');
 
+/*
+ * 利用自执行函数获取图片信息
+ */
 imgDatas = function(imgArr) {
 	for(var i=0, l=imgArr.length; i<l; i++){
 		var img = imgArr[i];
@@ -14,37 +17,44 @@ imgDatas = function(imgArr) {
 	return imgArr;
 }(imgDatas);
 
-class ImaFigure extends React.Component {
+/*
+ * ImgFigure组件
+ */
+class ImgFigure extends React.Component {
 	constructor(props) {
 		super(props);		
 	}
+
 	render() {
 		return (
-			<figure className="figure figure_front">
-				<div className="imgs front">
-					<img src={this.props.data.imgUrl} alt=""/>
-					<figcaption>
-						<h2>{this.props.data.caption}</h2>
-					</figcaption>
-				</div>
-				<div className="imgs back">
-					<p className="desc">{this.props.data.desc}</p>
-				</div>
+			<figure className="img_figure">
+				<img src={this.props.data.imgUrl} alt=""/>
+				<figcaption>
+					<h2 className="img_title">{this.props.data.caption}</h2>
+					<div className="img_back"><p>{this.props.data.desc}</p></div>
+				</figcaption>				
 			</figure>
 		);
 	}
 }
 
+/*
+ * AppComponent组件
+ */
 class AppComponent extends React.Component {
+  constructor(props) {
+  	super(props);
+  }
+
   render() {
   	var imgFigures = [];
   	imgDatas.forEach(function(value, index) {
-  		imgFigures.push(<ImaFigure data={value} key={'img_' + index} />);
+  		imgFigures.push(<ImgFigure data={value} key={'img_' + index} />);
   	});
 
     return (
       <section className="stage">
-      	<section className="imgFigure">{imgFigures}</section>
+      	<section className="img_sec">{imgFigures}</section>
       </section>
     );
   }
